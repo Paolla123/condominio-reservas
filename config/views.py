ae6920eb50e972c5d31e5c0b50b58abf64ef25f3
LuiscffBRA <luiscff2001@hotmail.com>
2026-03-14 19:13:11 -0300

    Adicionando templates de home

--- config/views.py
+++ config/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def login_view(request):
     return render(request, 'login.html')
+
+def home_morador(request):
+    return render(request, 'home_morador.html')
+
+def home_admin(request):
+    return render(request, 'home_admin.html')
+
+def home_sindico(request):
+    return render(request, 'home_sindico.html')
